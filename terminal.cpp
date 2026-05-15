5091e781185e47b5e4f784efa332a7ab53dfd596
Harshavardhana Shastri <harshavardhana.shastri@velocomputing.tech>
2026-05-15 12:58:31 +0200

    Remove empty line in terminal.cpp

--- terminal.cpp
+++ terminal.cpp
@@ -5,7 +5,6 @@ void render_terminal(void)
     draw_rect(40, 30, 240, 140, 0);  /* black terminal background */
     draw_rect(40, 30, 240, 15, 7);   /* grey title bar */
     draw_text(45, 33, "Terminal", 0);
-
     /* Fake cursor prompt */
     draw_text(45, 52, "> _", 2);
 }
